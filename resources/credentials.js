ff6ffb9b678b66ebf51cd4c1c377c41b75fef0b4
Oliver Bucaojit <oliver.bucaojit@mongodb.com>
2023-02-13 18:52:00 +0000

    SQL-1126: Implement PowerBI Integration Testing

---
+++ resources/credentials.js
@@ -0,0 +1,9 @@
+{
+                "AuthenticationKind": "UsernamePassword",
+                "AuthenticationProperties": {
+                    "Username": "$ADL_TEST_LOCAL_USER",
+                    "Password": "$ADL_TEST_LOCAL_PWD"
+                },
+                "PrivacySetting": "None",
+                "Permissions": []
+}
